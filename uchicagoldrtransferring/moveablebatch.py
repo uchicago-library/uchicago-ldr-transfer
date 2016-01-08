86eb1e351061e6f4b577a562e9020be44591044e
Tyler Danstrom <tdanstrom@uchicago.edu>
2016-01-08 16:39:51 -0600

    added moveabledirectory and stagingdirectory class

--- uchicagoldrtransferring/moveablebatch.py
+++ uchicagoldrtransferring/moveablebatch.py
@@ -1,30 +1,85 @@
-
 from os import access, listdir, rmdir, R_OK
 from os.path import join, isfile, isdir, relpath
+from uchicagoldr.batch import Directory
+from uchicagoldrtransferring.moveableitem import MoveableItem
+from uchicagoldrconfig.LDRCofiguration import LDRConfiguration
 
-from uchicagoldrtransferring.moveableitem import Item
+config = LDRConfiguration('/home/tdanstrom/src/ldr_projects/uchicagoldr-config/').get_config()
 
-class Batch(object):
-    items = []
-    directory = ""
-    
-    def __init__(self, batch_directory, root_of_batch):
-        self.directory = batch_directory
-        directory_relative_to_root = relpath(self.directory, root_of_batch)
-        accession, *tail = directory_relative_to_root.split('/')
-        self.accession = accession
-        self.items = self.find_all_files(self.directory, root_of_batch)
-            
-    def find_all_files(self, directory, directory_root):
-        flat_list = listdir(directory)
-        for file_basename in flat_list:
-            fullpath = join(directory, file_basename)
+class StagingDirectory(MoveableDirectory):
+
+    def __init__(self, directory_path, source_root, destination_root, 
+                 ead_identifier, accession_number, embargo=False, items=None):
+        assert exists(directory_path)
+        assert re_compile('^\d{4}[-]\d{3}$').match(accession_number)
+        assert re_compile('^ICU.(.*)$').match(ead_identifier)
+        self.ead_identifier = ead_identifier
+        self.accession_number = accession_number
+        self.accession_noid = self.mint_accession_identifier()
+        if embargo:
+            self.user = "embargo"
+            self.group = "embargo"
+        else:
+            self.user = "repository"
+            self.group = "repository"
+        self.directory_path = directory_path
+        self.items = self.walk_directory_picking_files(directory_path)
+
+    def mint_accession_identifier(self):
+        url_data = config['accessionminter']['url']
+        if url_data.status == 200:
+            url_data = url_data.read()
+        else:
+            raise ValueError("Could not fetch batch identifier from " +
+                             "RESTful NOID minter")
+        return url_data.split('61001/').rstrip()
+        
+
+    def ingest():
+        for n in self.items:
+            i.copy_into_new_location()
+        
+class MoveableDirectory(Directory):
+
+    def __init__(self, directory_path, source_root, destination_root, 
+                 items=None):
+        self.directory_path = directory_path
+        self.items = self.walk_directory_picking_files(source_root, 
+                                                       destination_root)
+
+    def walk_directory_picking_files(self):
+        flat_list = listdir(self.get_directory_path())
+        while flat_list:
+            node = flat_list.pop()
+            fullpath = join(self.get_directory_path(), node)
             if isfile(fullpath):
-                i = Item(fullpath, directory_root)
+                i = MoveableItem(fullpath, source_root, destination_root)
                 yield i
             elif isdir(fullpath):
-                for n in self.find_all_files(fullpath, directory_root):
-                    yield n
+                for child in listdir(fullpath):
+                    flat_list.append(join(fullpath, child))
 
-    def clean_out_batch(self):
-        rmdir(self.directory)
+# class Batch(object):
+#     items = []
+#     directory = ""
+    
+#     def __init__(self, batch_directory, root_of_batch):
+#         self.directory = batch_directory
+#         directory_relative_to_root = relpath(self.directory, root_of_batch)
+#         accession, *tail = directory_relative_to_root.split('/')
+#         self.accession = accession
+#         self.items = self.find_all_files(self.directory, root_of_batch)
+            
+#     def find_all_files(self, directory, directory_root):
+#         flat_list = listdir(directory)
+#         for file_basename in flat_list:
+#             fullpath = join(directory, file_basename)
+#             if isfile(fullpath):
+#                 i = Item(fullpath, directory_root)
+#                 yield i
+#             elif isdir(fullpath):
+#                 for n in self.find_all_files(fullpath, directory_root):
+#                     yield n
+
+#     def clean_out_batch(self):
+#         rmdir(self.directory)
--- uchicagoldrtransferring/moveableitem.py
+++ uchicagoldrtransferring/moveableitem.py
@@ -14,8 +14,8 @@ class MoveableItem(Item):
 
     def __init__(self, filepath, source_root, destination_root):
         assert exists(abspath(file_path))
-        assert not exists(abspath(destination_root))
-        asserts exists(abspath(source_root))
+        assert exists(abspath(destination_root))
+        assert exists(abspath(source_root))
         self.root_path = source_root
         self.destination_root = destination_root
         self.filepath = filepath
@@ -74,104 +74,3 @@ class MoveableItem(Item):
             return (True,None)
         except Exception as e:
             return (False,e)
-
-# class Item(object):
-#     root_path = ""
-#     filepath = ""
-#     destination = ""
-#     sha256_hash = ""
-#     md5_hash = ""
-#     accession = ""
-#     mimetype = ""
-#     can_read = False
-#     has_technical_md = False
-    
-#     def __init__(self, path, root):
-#         if access(path, R_OK):
-#             self.can_read = True
-#         else:
-#             pass
-#         self.root_path = root
-#         self.filepath = path
-#         self.find_file_accession()
-#         self.get_file_mime_type()
-#         self.get_file_size()
-#         self.sha256_hash = self.get_file_hash(sha256)
-#         self.md5_hash = self.get_file_hash(md5)
-#         self.find_technical_metadata()
-        
-#     def get_file_hash(self, hash_type, blocksize=65536):
-#         blocksize = 65536
-#         hash = hash_type()
-#         afile = open(self.filepath,'rb')
-#         buf = afile.read(blocksize)
-#         while len(buf) > 0:
-#             hash.update(buf)
-#             buf = afile.read(blocksize)
-#         return hash.hexdigest()
-
-#     def find_file_accession(self):
-#         relative_path = relpath(self.filepath, self.root_path)
-#         accession, *tail = relative_path.split('/')
-#         self.accession = accession
-        
-#     def get_file_size(self):
-#         self.file_size = stat(self.filepath).st_size        
-#         return True
-        
-#     def get_file_mime_type(self):
-#         self.mimetype = guess_type(self.filepath)[0]
-#         return True
-    
-#     def find_technical_metadata(self):
-#         fits_filepath = join(self.filepath,'.fits.xml')
-#         if exists(fits_filepath):
-#             self.has_technical_md = True
-#         else:
-#             pass
-#         return True
-
-#     def set_destination_path(self, new_root_directory):
-#         path_sans_root = relpath(self.filepath, self.root_path)
-#         destination_full_path = join(new_root_directory, path_sans_root)
-#         self.destination = destination_full_path
-#         return True
-    
-#     def move_into_new_location(self):
-#         try:
-#             move(self.filepath, self.destination)
-#             return (True,None)
-#         except Exception as e:
-#             error = e
-#             return (False,e)
-        
-#     def copy_source_directory_tree_to_destination(self):
-#         destination_directories = dirname(self.destination).split('/')
-#         directory_tree = ""
-#         for f in destination_directories:
-#             directory_tree = join(directory_tree,f)
-#             if not exists(directory_tree):
-#                 try:
-#                     mkdir(directory_tree,0o740)
-#                 except Exception as e:
-#                     return (False,e)
-#         return (True,None)
-    
-#     def clean_out_source_directory_tree(self):
-#         directory_tree = dirname(self.filepath)
-#         for src_dir, dirs, files in walk(directory_tree):
-#             try:
-#                 rmdir(src_dir)
-#                 return (True,None)
-#             except Exception as e:
-#                 return (False,e)
-    
-#     def set_destination_ownership(self, user_name, group_name):
-#         uid = getpwnam(user_name).pw_uid
-#         gid = getgrnam(group_name).gr_gid
-#         try:
-#             chown(self.destination, uid, gid)
-#             return (True,None)
-#         except Exception as e:
-#             return (False,e)
-    
